cbe880d870d136bff34e36eacad44e94d5367ec8
GAUTAM AGRAWAL <gautamagrawal@GAUTAMs-MacBook-Air.local>
2026-01-03 20:49:46 +0530

    Fix ESLint import order in GeneralContext

--- dashboardnew/src/components/GeneralContext.js
+++ dashboardnew/src/components/GeneralContext.js
@@ -1,12 +1,11 @@
-export const BACKEND_URL =
-  process.env.REACT_APP_BACKEND_URL || "http://localhost:3002";
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { watchlist } from "../data/data";
 
 import BuyActionWindow from "./BuyActionWindow";
 import SellActionWindow from "./SellActionWindow";
+export const BACKEND_URL =
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:3002";
 
 const GeneralContext = React.createContext();
 
@@ -47,7 +46,7 @@ export const GeneralContextProvider = ({ children }) => {
   // ---------------- HOLDINGS FETCH ----------------
   const fetchHoldings = async () => {
     try {
-      const res = await axios.get(`${BACKEND_URL}/allHoldings`);
+      const res = await axios.get("http://localhost:3002/allHoldings");
       setAllHoldings(Array.isArray(res.data) ? res.data : []);
     } catch {
       setAllHoldings([]);
