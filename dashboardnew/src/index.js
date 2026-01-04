5c7576cb86c88842738dee2cadd2c4cf5586468d
GAUTAM AGRAWAL <gautamagrawal@GAUTAMs-MacBook-Air.local>
2026-01-04 15:55:20 +0530

    Fix ESLint import order in index.js

--- dashboardnew/src/index.js
+++ dashboardnew/src/index.js
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
-
+import axios from "axios";
 import { GeneralContextProvider } from "./components/GeneralContext";
 import Apps from "./components/Apps";
-import axios from "axios";
-
-axios.defaults.withCredentials = true;
-
 import "./index.css";
-
+axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
